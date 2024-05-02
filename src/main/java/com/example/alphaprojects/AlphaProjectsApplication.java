6d39082d6c7212a84b9d59564d85bd7c1d42bcfa
UsmanSharif0001 <ussh0001@kea.stud.dk>
2024-05-02 09:54:47 +0200

    Initial commit

---
+++ src/main/java/com/example/alphaprojects/AlphaProjectsApplication.java
@@ -0,0 +1,13 @@
+package com.example.alphaprojects;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlphaProjectsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlphaProjectsApplication.class, args);
+    }
+
+}
