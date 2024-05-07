eb8fdf31d7f58d51cec582ef89a399e9e97d9d20
NikoP93 <nipe0001@stud.kea.dk>
2024-05-07 18:29:19 +0200

    Made Controller calls for login. Made html for login

--- src/main/java/com/example/alphaprojects/controllers/EmpController.java
+++ src/main/java/com/example/alphaprojects/controllers/EmpController.java
@@ -1,9 +1,15 @@
 package com.example.alphaprojects.controllers;
 
 
+import com.example.alphaprojects.model.Emp;
 import com.example.alphaprojects.services.EmpService;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("")
@@ -13,6 +19,32 @@ public class EmpController {
     public EmpController(EmpService empService) {
         this.empService = empService;
     }
+
+    private boolean isLoggedIn(HttpSession session) {
+        return session.getAttribute("user") != null;
+    }
+
+    /*-----------------------------Login--------------------------*/
+
+    @GetMapping()
+    public String loginForm(HttpSession session) {
+        session.invalidate();
+        return "login";
+    }
+
+    @PostMapping("/login")
+    public String login(@RequestParam String email, @RequestParam String password,
+                        HttpSession session, Model model) {
+        Emp emp = empService.getEmp(email,password);
+        if (emp != null){
+            session.setAttribute("emp", emp);
+            session.setMaxInactiveInterval(1200);
+            return "redirect:/projekter";
+        }
+        model.addAttribute("wrongCredentials",true);
+        return "login";
+    }
+
 
 
 }
