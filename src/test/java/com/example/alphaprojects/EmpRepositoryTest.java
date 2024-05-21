c17050d6893fbd871ed2a0a7ae6875e9cafd59c1
NikoP93 <nipe0001@stud.kea.dk>
2024-05-21 13:44:00 +0200

    Order in test

--- src/test/java/com/example/alphaprojects/EmpRepositoryTest.java
+++ src/test/java/com/example/alphaprojects/EmpRepositoryTest.java
@@ -11,7 +11,6 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
 
 import java.util.ArrayList;
