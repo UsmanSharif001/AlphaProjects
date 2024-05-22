ebdb1fd51cbbd3a36f638446eaca6e63a83421ef
UsmanSharif0001 <ussh0001@kea.stud.dk>
2024-05-22 10:29:56 +0200

    edited in yml file

--- src/test/java/com/example/alphaprojects/TaskRepositoryTest.java
+++ src/test/java/com/example/alphaprojects/TaskRepositoryTest.java
@@ -11,9 +11,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
-
 import java.time.LocalDate;
 import java.util.List;
 
